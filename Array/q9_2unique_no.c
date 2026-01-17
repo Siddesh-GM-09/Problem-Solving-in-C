12a4128034b58500d1c0260c8d2fbb6aab469848
Siddesh-GM-09 <siddesh.gm25@gmail.com>
2026-01-17 21:28:07 +0530

    2 Unique Numbers Updated

--- Array/q9_2unique_no.c
+++ Array/q9_2unique_no.c
@@ -7,7 +7,7 @@ You are given an integer array where:
 #include<stdlib.h>
 void findTwoUnique(int *arr, int size, int *x, int *y);
 int main(){
-    int size,x,y;
+    int size,x=0,y=0;
     printf("Enter the size : ");
     scanf("%d",&size);
     int *arr=malloc(sizeof(int)*size);
@@ -16,8 +16,21 @@ int main(){
         scanf("%d",&arr[i]);
     }
     findTwoUnique(arr,size,&x,&y);
-
+    printf("Numbers are : %d & %d\n",x,y);
+    free(arr);
 }
 void findTwoUnique(int *arr, int size, int *x, int *y){
-    
+    int result=0;
+    for(int i=0;i<size;i++){
+        result^=arr[i];
+    }
+    result=result&(-result);
+    for(int i=0;i<size;i++){
+        if(result&(arr[i])){
+            *x^=arr[i];
+        }
+        else{
+            *y^=arr[i];
+        }
+    }
 }
